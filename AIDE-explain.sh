9f3e153fd787727ab528260c35baff0cce0f8216
migalys <migalys@gmail.com>
2019-08-18 13:37:18 +1000

    first commit

---
+++ AIDE-explain.sh
@@ -0,0 +1,108 @@
+######### Migalys Pavon, 2018-08-13 AIDE-explain.sh ####################
+#!/bin/bash
+
+
+#### Scrip to check changes reported by AIDE and return ####
+#####        Date and time of AIDE execution          ######
+#####        Name of files that have been added       ######
+#####        Name of files that have been removed     ######
+#####        Name of files that have changed          ######
+
+#cheking for new files
+new_file() {
+    new=$(echo "$aide_output" | grep "f+++++++++" | cut -d' ' -f2)
+    new_count=$(echo "$new" | grep -cv ^\$)
+    if [[ ${new_count} -gt 0 ]]; then
+        echo -e "\n${new_count} Files have been added"
+        echo "${new}"
+    else
+        echo -e "\nNo files were added"
+    fi
+}
+
+#checking for removed files
+removed_file() {
+    removed=$(echo "$aide_output" | grep "f-----------" | cut -d' ' -f2)
+    remv_count=$(echo "$removed" | grep -cv ^\$)
+    if [[ ${remv_count} -gt 0 ]]; then
+        echo -e "\n${remv_count} Files have been removed"
+        echo  "${removed}"
+    else
+        echo -e "\nNo files were removed"
+    fi
+}
+
+#checking for changed files
+#determine if change is in content, size or permissions
+changed_file() {
+    content=$(echo "$aide_output" | grep "f [=<>] " | cut -d' ' -f9)
+    cont_count=$(echo "$content" | grep -cv ^\$)
+    if [[ ${cont_count} -gt 0 ]]; then
+        echo -e "\n${cont_count} Files have been changed."
+        echo  "${content}"
+    else
+        echo -e "\nNo files have been changed"
+    fi
+
+
+    time=$(echo "$aide_output" | grep "^f\s[<>=]\s...\smc" | cut -d' ' -f9)
+    cont_count=$(echo "$time" | grep -cv ^\$)
+    if [[ ${cont_count} -gt 0 ]]; then
+        echo -e "\n${cont_count} Files have the modification and change time changed."
+        echo  "${time}"
+    else
+        echo -e "\nNo files modification and change time were changed"
+    fi
+
+
+    md=$(echo "$aide_output" | grep "^f\s[<>=]\s...\s..\s.C" | cut -d' ' -f9)
+    cont_count=$(echo "$md" | grep -cv ^\$)
+    if [[ ${cont_count} -gt 0 ]]; then
+        echo -e "\n${cont_count} Files have the checksums changed."
+        echo  "${md}"
+    else
+        echo -e "\nNo files checksums were changed"
+    fi
+
+    perm=$(echo "$aide_output" | grep "f [=<>] p" | cut -d' ' -f9)
+    perm_count=$(echo "$perm" | grep -cv ^\$)
+    if [[ ${perm_count} -gt 0 ]]; then
+        echo -e "\n${perm_count} Files have permission changed"
+        echo  "${perm}"
+    else
+        echo -e "\nNo files permissions were changed"
+    fi
+
+    size=$(echo "$aide_output" | grep "f [<>]" | cut -d' ' -f9)
+    size_count=$(echo "$size" | grep -cv ^\$)
+    if [[ ${size_count} -gt 0 ]]; then
+        echo -e "\n${size_count} Files have the size changed"
+        echo "${size}"
+    else
+        echo -e "\nNo files sizes were changed"
+    fi
+}
+
+#check if AIDE is installed
+if ! aide -version 2>/dev/null; then
+    echo "AIDE is not installed, please."
+    exit
+fi
+
+#check if there are changes
+if aide_output=$(aide --config=/etc/aide/aide.conf --check); then
+	  echo "AIDE detected NO changes."
+else
+    EXIT_STATUS=$?
+    if  [[ "${EXIT_STATUS}" -le 7 && "${EXIT_STATUS}" -ge 1 ]]; then
+        echo -e "\nDate and time: $(echo "$aide_output" | grep "Start timestamp:" | cut -d' ' -f3-4)"
+        echo -e "\nAIDE detected changes"
+
+        new_file
+        removed_file
+        changed_file
+
+    else
+        echo "Something was wrong with AIDE check."
+    fi
+fi
---
+++ checkIntegrity.sh
@@ -0,0 +1,141 @@
+######### Migalys Pavon, 2018-08-13 checkingIntegrity.sh ####################
+#!/usr/bin/bash
+
+
+####################### To install and configure AIDE ########################
+##### Check whether AIDE is installed. If not, install and configure it ######
+##############################################################################
+error_exit()
+{
+    echo "${1}" 1>&2
+    exit 1
+}
+
+install_aide()
+{
+apt update
+
+apt install aide
+
+# update configuration file
+if cd /etc/aide; then
+    rm aide.conf
+    touch aide.conf
+    echo -e '#Path for creating the databases\ndatabase=file:/var/lib/aide/aide.db\ndatabase_out=file:/var/lib/aide/aide.db.new\ndatabase_new=file:/var/lib/aideaide.db.new\n\n# Set your own AIDE rule\nMYRULE = p+n+u+g+s+m+c+xattrs+md5+sha512\n\n# Directories/files to be monitored and rule to apply\n/etc MYRULE\n/bin MYRULE\n/usr/bin MYRULE\n/root MYRULE\n\n# Directories to ignore\n!/home\n!/proc' > aide.conf
+else
+    error_exit 'Something was wrong with the installation.'
+fi
+
+#init aide to create databases
+aide --config=/etc/aide/aide.conf --init
+
+if cd /var/lib/aide; then
+    cp aide.db.new  aide.db
+else
+    error_exit 'Something was wrong with the configuration.'
+fi
+}
+
+#call function if aide is not installed
+if ! aide -version 2>/dev/null; then
+    install_aide
+fi
+
+################## end install and configure AIDE #####################
+#######################################################################
+
+################# creating new folder and file to check AIDE ##########
+#######################################################################
+
+to_check_aide() {
+    if [[ -e "/etc/aide-test" ]]; then
+        echo 'aide-test folder already exists'
+    else
+        mkdir /etc/aide-test
+    fi
+
+    if [[ -f "/etc/security-alert" ]]; then
+        echo 'file security-alert already exists'
+    else
+        touch /etc/security-alert
+    fi
+}
+
+
+## call function to_check_aide if necessary to generate a folder and file ##
+####### Comment it out not to create a folder and file to test AIDE ########
+
+to_check_aide
+
+############## end creating new folder and file to check AIDE #########
+#######################################################################
+
+
+################# check for changes in the system ####################
+######################################################################
+
+if ! aide --config=/etc/aide/aide.conf --check; then
+    # AIDE detected changes
+    EXIT_STATUS=$?
+    if  [[ "${EXIT_STATUS}" -gt 7 ]]; then
+        case ${EXIT_STATUS} in
+            14)
+                message="Writing error."
+                ;;
+            15)
+                message="Invalid argument error."
+                ;;
+            16)
+                message="Unimplemented function erro.r"
+                ;;
+            17)
+                message="Invalid configure line error."
+                ;;
+            18)
+                message="IO error."
+                ;;
+            19)
+                message="Version mismatch error."
+                ;;
+            *)
+                message="Something was wrong with AIDE."
+                ;;
+        esac
+    else
+        message="AIDE detected changes in the system!"
+    fi
+
+    #Check if user exist and send mail with alert or error
+    list=''
+    date=$(date +'%d-%m-%Y %X')
+
+    #admin users to send alert if no user
+    adm="root $(getent group sudo | cut -d: -f4 | cut -d, -f1,2 --output-delimiter ' ')"
+
+    for arg in "$@"
+    do
+        if getent passwd "$arg" >>/dev/null; then
+            list+=" $arg"
+        fi
+    done
+
+    if [[ -n "$list" ]]; then
+        echo "${message} on ${date}." | mailx ${list} -s "AIDE alert on ${date}"
+        if [[ $? -ne 0 ]]; then
+            echo "Unable to send email to ${list} with aler: ${message}."
+        else
+            echo "AIDE alert was sent to ${list}: ${message}."
+        fi
+    else
+        echo "There were no valid users to send AIDE alert on ${date}." | mailx ${adm} -s "AIDE alert error on ${date}"
+        if [[ $? -ne 0 ]]; then
+            echo "Unable to send email to admins with aler: ${message}."
+        else
+            echo "There were no valid users to send AIDE alert: ${message}."
+        fi
+    fi
+else
+    echo "AIDE did not report changes."
+fi
+#################             end                  ####################
+#######################################################################
